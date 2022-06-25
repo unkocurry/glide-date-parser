68ad06bac8bb3a75c365153f9cdbf7f95852d67a
unkocurry <108160052+unkocurry@users.noreply.github.com>
2022-06-24 18:57:07 -0600

    Update function.js

--- function.js
+++ function.js
@@ -2,20 +2,21 @@
 // have declared in the `glide.json` file, in the
 // same order.
 window.function = function (str, type) { 
-  // 2018-01-01T16:55:30.974Z
-  // January 1, 2017, 3:24PM
-  let peices = str.split(',')
-  let final = 'DEFAULT';
-  if (type === 'HOUR') {
-    // Later
-    final = 'not implemented yet'
-  } else if (type === 'DAY') {
-    final = peices[0].split(' ')[1]
-  } else if (type === 'MONTH') {
-    final = peices[0].split(' ')[0]
-  } else if (type === 'YEAR') {
-    final = peices[1].trim() 
-  }
+  return "NOTHING"
+//   // 2018-01-01T16:55:30.974Z
+//   // January 1, 2017, 3:24PM
+//   let peices = str.split(',')
+//   let final = 'DEFAULT';
+//   if (type === 'HOUR') {
+//     // Later
+//     final = 'not implemented yet'
+//   } else if (type === 'DAY') {
+//     final = peices[0].split(' ')[1]
+//   } else if (type === 'MONTH') {
+//     final = peices[0].split(' ')[0]
+//   } else if (type === 'YEAR') {
+//     final = peices[1].trim() 
+//   }
 
-  return final
+//   return final
 }
