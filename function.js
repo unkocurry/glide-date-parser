a2ba565083fee97881e8860da2ae2ac96574855a
unkocurry <108160052+unkocurry@users.noreply.github.com>
2022-06-24 19:00:45 -0600

    Update function.js

--- function.js
+++ function.js
@@ -1,12 +1,50 @@
-const functions = new Map();
-
-window.function = function (code, ...params) {
-  code = code.value;
-  if (code === undefined) return undefined;
-
-  const functionCode = `async (p1,p2,p3,p4,p5,p6,p7)=>{ ${code} }`;
-  const fn = eval(functionCode);
-  functions.set(code, fn);
-
-  return fn(...params.map(p => p.value));
+{
+  "name": "Hell Yes Code",
+  "description": "The yes-codest of columns.",
+  "author": "Mark Probst <mark@glideapps.com>",
+  "params": [
+    {
+      "name": "code",
+      "displayName": "Code",
+      "type": "string"
+    },
+    {
+      "name": "p1",
+      "displayName": "p1",
+      "type": "primitive"
+    },
+    {
+      "name": "p2",
+      "displayName": "p2",
+      "type": "primitive"
+    },
+    {
+      "name": "p3",
+      "displayName": "p3",
+      "type": "primitive"
+    },
+    {
+      "name": "p4",
+      "displayName": "p4",
+      "type": "primitive"
+    },
+    {
+      "name": "p5",
+      "displayName": "p5",
+      "type": "primitive"
+    },
+    {
+      "name": "p6",
+      "displayName": "p6",
+      "type": "primitive"
+    },
+    {
+      "name": "p7",
+      "displayName": "p7",
+      "type": "primitive"
+    }
+  ],
+  "result": {
+    "type": "primitive"
+  }
 }
