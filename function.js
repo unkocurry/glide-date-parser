a6d8d930f5c656c059d45d71e91f739447d47ea5
unkocurry <108160052+unkocurry@users.noreply.github.com>
2022-06-24 18:39:39 -0600

    Update function.js

--- function.js
+++ function.js
@@ -5,15 +5,15 @@ window.function = function (str, type) {
   // 2018-01-01T16:55:30.974Z
   // January 1, 2017, 3:24PM
   let peices = str.split(',')
-  let final;
-  if (type === "HOUR") {
+  let final = 'DEFAULT';
+  if (type === 1) {
     // Later
     final = 'not implemented yet'
-  } else if (type === "DAY") {
+  } else if (type === 2) {
     final = +peices[0].split(' ')[1]
-  } else if (type === "MONTH") {
+  } else if (type === 3) {
     final = peices[0].split(' ')[0]
-  } } else if (type === "YEAR") {
+  } } else if (type === 4) {
     final = +peices[1].trim() 
   }
 
